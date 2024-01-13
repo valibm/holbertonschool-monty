823dba239571089c1effdcc01ba247dff2769b04
zabitaguliyeva <7644@holbertonstudents.com.com>
2024-01-13 02:44:50 -0800

    Added check_ops function

--- helper_funcs.c
+++ helper_funcs.c
@@ -31,3 +31,42 @@ void handle_com(FILE *file, stack_t **stack)
 
 	free(opcode);
 }
+
+/**
+ * check_ops - Checks the opcode in the line
+ * @opcode: Requested function
+ * @stack: Pointer to the top of the stack
+ * @line: Line number
+ *
+ * Return: Function pointer or EXIT_FAILURE if it fails
+ */
+void check_ops(char *opcode, stack_t **stack, unsigned int line)
+{
+	int i = 0;
+
+	instruction_t opcodes[] = {
+		{"pall", pall},
+		{"pint",  pint},
+		{"pop", pop},
+		{"swap", swap},
+		{"add", add},
+		{"nop", nop},
+		{NULL, NULL}
+	};
+
+	while (opcodes[i].opcode)
+	{
+		if (strcmp(opcodes[i].opcode, opcode) == 0)
+		{
+			opcodes[i].f(stack, line);
+			break;
+		}
+		i++;
+	}
+
+	if (opcodes[i].opcode == NULL)
+	{
+		fprintf(stderr, "L%d: unknown instruction %s\n", line, opcode);
+		exit(EXIT_FAILURE);
+	}
+}
