04bdb05eeb1ed22a6626f0114ec0316bbcc71ad3
zabitaguliyeva <7644@holbertonstudents.com.com>
2024-01-13 02:55:34 -0800

    Added pop function

--- stack_funcs.c
+++ stack_funcs.c
@@ -106,3 +106,29 @@ void pint(stack_t **stack, unsigned int line)
 
 	printf("%d\n", tmp->n);
 }
+
+/**
+ * pop - Removes the top element of the stack.
+ * @stack: Pointer to the top of a stack.
+ * @line: Number of the line.
+ *
+ * Retunr: Void.
+ */
+void pop(stack_t **stack, unsigned int line)
+{
+	stack_t *top;
+
+	top = *stack;
+
+	if (!stack || !*stack)
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line);
+		exit(EXIT_FAILURE);
+	}
+
+	*stack = top->next;
+	if (*stack != NULL)
+		(*stack)->prev = NULL;
+
+	free(top);
+}
