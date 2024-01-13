6fd40ace85ef8a344538207b732db1cab9024d4a
root <valibm>
2024-01-13 02:24:22 -0800

    Added the swap function

--- stack_funcs.c
+++ stack_funcs.c
@@ -34,3 +34,31 @@ void push(char *opcode, stack_t **top, unsigned int line)
 		(*top)->prev = stack;
 	*top = stack;
 }
+
+
+/**
+ * swap - Swaps the top two elements of the stack.
+ * @stack: Pointer to the top of a stack.
+ * @line: Number of the line.
+ *
+ * Return: Void.
+ */
+void swap(stack_t **stack, unsigned int line)
+{
+	stack_t *tmp;
+	stack_t *node;
+
+	tmp = *stack;
+	if (!*stack || !(*stack)->next)
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line);
+		exit(EXIT_FAILURE);
+	}
+
+	node = tmp->next;
+	tmp->next = node->next;
+	node->next = *stack;
+	tmp->prev = node;
+	node->prev = NULL;
+	*stack = node;
+}
