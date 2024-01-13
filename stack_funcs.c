4f7564824b5ed681eac85660a80ee14ff8a30190
zabitaguliyeva <7644@holbertonstudents.com.com>
2024-01-13 02:54:03 -0800

    Added pint function

--- stack_funcs.c
+++ stack_funcs.c
@@ -85,3 +85,24 @@ void pall(stack_t **stack, unsigned int __attribute__((unused)) line)
 		tmp = tmp->next;
 	}
 }
+
+/**
+ * pint - Prints the value at the top of the stack, followed by a new line.
+ * @stack: Pointer to the top of a stack.
+ * @line: Number of the line.
+ * Return: Void.
+ */
+void pint(stack_t **stack, unsigned int line)
+{
+	stack_t *tmp;
+
+	tmp = *stack;
+
+	if (!stack || !*stack)
+	{
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line);
+		exit(EXIT_FAILURE);
+	}
+
+	printf("%d\n", tmp->n);
+}
