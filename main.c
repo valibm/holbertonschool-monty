7379f21b4987ef3a92a57c36a75542bfe0169311
root <valibm>
2024-01-13 02:15:30 -0800

    Added the main function

---
+++ main.c
@@ -0,0 +1,35 @@
+#include "monty.h"
+
+/**
+ * main - Monty bite code files interpreter
+ * @argc: Argument counter
+ * @argv: Argument vector
+ *
+ * Return: Void
+ */
+int main(int argc, char *argv[])
+{
+	stack_t *stack = NULL;
+	FILE *fd;
+	char *file_n;
+
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+
+	file_n = argv[1];
+	fd = fopen(file_n, "r");
+	if (fd == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", file_n);
+		exit(EXIT_FAILURE);
+	}
+
+	handle_com(fd, &stack);
+	free_stack(stack);
+	fclose(fd);
+
+	exit(EXIT_SUCCESS);
+}
