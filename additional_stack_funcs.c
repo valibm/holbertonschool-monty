9f2da0cd27aa3310a94bc37673d36604d7b20e01
zabitaguliyeva <7644@holbertonstudents.com.com>
2024-01-13 02:37:53 -0800

    Added add function

--- additional_stack_funcs.c
+++ additional_stack_funcs.c
@@ -1,6 +1,31 @@
 #include "monty.h"
 
 /**
+ * add - Adds the top two elements of the stack
+ * @stack: Pointer to the top of a stack.
+ * @line: Number of a line.
+ *
+ * Return: Void.
+ */
+void add(stack_t **stack, unsigned int line)
+{
+	int addition;
+	stack_t *tmp;
+
+	tmp = *stack;
+	if (!*stack || !(*stack)->next)
+	{
+		fprintf(stderr, "L%d: can't add, stack too short\n", line);
+		exit(EXIT_FAILURE);
+	}
+
+        addition = tmp->n + tmp->next->n;
+	pop(stack, line);
+
+	(*stack)->n = addition;
+}
+
+/**
  * nop - Doesn’t do anything.
  * @stack: Pointer to the top of a line.
  * @line: Number of a line.
